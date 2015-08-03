e4a8cde7bf2856f2d3fdbe6e554bd623806d7eb4
jameshintaro <jamesoda28@gmail.com>
2015-08-03 19:32:06 +0900

    Create refresh.el

---
+++ refresh.el
@@ -0,0 +1,11 @@
+;; revert-buffer-no-confirm
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  ;;(message "force-reverting value is %s" force-reverting)
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
